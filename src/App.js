44eeb7819d1cc7d8132f3221dba0a68bc365134a
Jeremy King <jeremyking@Jeremys-Air.fios-router.home>
2021-04-13 19:15:14 -0400

    working on functionality of adding and removing items from the cart

--- src/App.js
+++ src/App.js
@@ -16,16 +16,22 @@ const fetchCart = async () => {
     setCart(await commerce.cart.retrieve());
 }
 
+const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+}
+
+
 useEffect(() => {
     fetchProducts();
+    fetchCart();
 }, [])
-
-console.log(products);
 
     return (
         <div>
-            <Navbar />
-           <Products products ={ products }/>
+            <Navbar totalItems = {cart.total_items } />
+           <Products products ={ products } onAddToCart ={handleAddToCart}/>
         </div>
     )
 }
